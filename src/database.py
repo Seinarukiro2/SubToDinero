00ee46dd4982949ced51d2f9f9c35aaf78caf7f6
Seinarukiro2 <seinarukiro@gmail.com>
2024-01-15 20:21:22 +0100

    to subscribe channels was added to database

--- src/database.py
+++ src/database.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
+    # Default user columns 
     id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(Integer, unique=True, index=True)
     username = Column(String, index=True, nullable=True)
@@ -18,18 +19,26 @@ class User(Base):
     source_start = Column(String, nullable=True)
     is_admin_session_active = Column(Boolean, default=False)
 
-    # Добавляем поля подписки на каналы
+    # To subscribe channels json
+    subscribed_channels = Column(JSON, default=[])
+
+    # Access channels
     subscribed_hmelnytskyi = Column(Boolean, default=False)
     subscribed_lutsk = Column(Boolean, default=False)
     subscribed_kiev = Column(Boolean, default=False)
     subscribed_ternopil = Column(Boolean, default=False)
 
-    # Добавляем поле, отслеживающее подписку на все каналы
+    # If user subscribed to all access channels
     subscribed_all = Column(Boolean, default=False)
 
-    # Обновляем subscribed_all при изменении других полей
     def update_subscribed_all(self):
         self.subscribed_all = all(getattr(self, f"subscribed_{channel_name}") for channel_name in ["hmelnytskyi", "lutsk", "kiev", "ternopil"])
+
+class ToSubscripeChannel(Base):
+    __tablename__ = 'to_subscribe_channels'
+
+    name = Column(String, unique=True, index=True)
+    link = Column(String)
 
 class Channel(Base):
     __tablename__ = 'channels'
@@ -43,14 +52,21 @@ Base.metadata.create_all(bind=engine)
 
 # Дефолтные записи перед созданием таблиц
 default_channels = [
-    Channel(id=1949156593, name='Хмельницкий', link='https://t.me/+tLXxnvJlzZo0M2Ey'),
-    Channel(id=1850257894, name='Луцьк', link='https://t.me/+jNKHP_USSJZhNzgy'),
-    Channel(id=1677904749, name='Киев', link='https://t.me/+cG_xCnSl7thiMzgy'),
-    Channel(id=1607603790, name='Тернополь', link='https://t.me/+oGriP3680ec1NDVi'),
+    {"id": 1949156593, "name": 'Хмельницкий', "link": 'https://t.me/+tLXxnvJlzZo0M2Ey'},
+    {"id": 1850257894, "name": 'Луцьк', "link": 'https://t.me/+jNKHP_USSJZhNzgy'},
+    {"id": 1677904749, "name": 'Киев', "link": 'https://t.me/+cG_xCnSl7thiMzgy'},
+    {"id": 1607603790, "name": 'Тернополь', "link": 'https://t.me/+oGriP3680ec1NDVi'},
 ]
 
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
-session.add_all(default_channels)
+
+for channel_data in default_channels:
+    channel_exists = session.query(Channel).filter_by(id=channel_data["id"]).first()
+
+    if not channel_exists:
+        new_channel = Channel(**channel_data)
+        session.add(new_channel)
+
 session.commit()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
